944855f46277d12f9e7b79b394fe81415d92ca46
Mike <reinstein.mike@gmail.com>
2024-09-02 11:04:03 -0700

    fix broken midpoint refactor when switching to wgpu

--- midpoint.js
+++ midpoint.js
@@ -5,7 +5,7 @@ import vec2Truncate from './truncate.js'
 export default function segmentMidPoint (out, p0, p1) {
     vec2.subtract(p1, p0, out)
     const halfLen = vec2.distance(p1, p0) / 2
-    vec2Truncate(out, halfLen, out)
+    vec2Truncate(out, out, halfLen)
     return vec2.add(out, p0, out)
 }
 
